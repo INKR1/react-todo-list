9bbf0fae6f86ab809a83ac5be65c4428f9d19510
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-15 16:20:04 +0300

    Created add todo component using useState hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import React from 'react';
 import Title from './components/Title';
+import AddTodo from './components/AddTodo';
 
 function App() {
   return (
     <div className="App">
       <div>
         <Title />
+      </div>
+      <div>
+        <AddTodo />
       </div>
     </div>
   );
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
   return (
     <div className='title'>
-        <h1>Todo app</h1>
+        <h1>Todo List</h1>
     </div>
   );
 }
