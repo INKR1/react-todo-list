a3dda55bf7c629d75ad8d253e1f656002560e1d8
Ineta-Kreiviene <ineta.kreiviene@icloud.com>
2022-07-15 15:09:06 +0300

    created title component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import React from 'react';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Title />
+      </div>
     </div>
   );
 }
